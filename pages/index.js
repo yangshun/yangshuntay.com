f3f513b57bdac8e4eca28b8c70c56f4b23a19c3f
Yangshun Tay <tay.yang.shun@gmail.com>
2019-06-02 16:15:43 -0700

    Get back my index page

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,24 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import withBaseUrl from '@docusaurus/withBaseUrl';
 import styles from './styles.module.css';
 
+function intersperse(items, separator) {
+  if (items.length < 1) {
+    return items;
+  }
+
+  const arr = [items[0]];
+  for (let i = 1; i < items.length; i++) {
+    arr.push(separator);
+    arr.push(items[i]);
+  }
+
+  return arr;
+}
+
 function Home() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
+  const { internetProfiles } = siteConfig.themeConfig;
   return (
     <Layout title={`${siteConfig.title} | Personal Homepage`}>
       <header className={classnames('hero', styles.header)}>
@@ -22,6 +37,19 @@ function Home() {
             }}
           />
           <h1 className="hero__title">{siteConfig.title}</h1>
+          <div className={styles.internetProfiles}>
+            {intersperse(
+              Object.keys(internetProfiles).map(key => {
+                const item = internetProfiles[key];
+                return (
+                  <a href={item.href} rel="noreferrer noopener" target="_blank">
+                    {item.label}
+                  </a>
+                );
+              }),
+              <span className="padding-horiz--md">|</span>,
+            )}
+          </div>
         </div>
       </header>
     </Layout>
